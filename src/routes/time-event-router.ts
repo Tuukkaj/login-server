10318fb148fd505477524b0cc2162d82f4e0b631
Tuukka Juusela <36623137+Tuukkaj@users.noreply.github.com>
2021-12-07 19:46:07 +0200

    Add basic db structure (#1)
    Adds basic DB structure with Sequelize

--- src/routes/time-event-router.ts
+++ src/routes/time-event-router.ts
@@ -1,9 +1,9 @@
-import express from "express"; 
+import express from "express";
 
 const timeEventRouter = express.Router();
 
 timeEventRouter.get("/", (req, res) => {
-    return res.send([]); 
+  return res.send([]);
 });
 
-export default timeEventRouter; 
+export default timeEventRouter;
